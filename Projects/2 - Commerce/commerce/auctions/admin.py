319de0ba3fffdd43931be3530cd863404acbd506
Mushinako <8977737+Mushinako@users.noreply.github.com>
2020-10-19 14:34:15 -0700

    2 step 01

--- Projects/2 - Commerce/commerce/auctions/admin.py
+++ Projects/2 - Commerce/commerce/auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Listing, Comment, Bid
+
 # Register your models here.
+for model in (Listing, Comment, Bid):
+    admin.site.register(model)
--- Projects/2 - Commerce/commerce/auctions/models.py
+++ Projects/2 - Commerce/commerce/auctions/models.py
@@ -3,4 +3,22 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self) -> str:
+        return f"{self.username}"
+
+
+class Listing(models.Model):
+    active = models.BooleanField()
+
+    def __str__(self) -> str:
+        return f"{self.id}: {self.active}"
+
+
+class Bid(models.Model):
+    def __str__(self) -> str:
+        return f"{self.id}: {self}"
+
+
+class Comment(models.Model):
+    def __str__(self) -> str:
+        return f"{self.id}: {self}"
