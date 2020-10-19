e5c34ba785ba064f4cd6028e28c846d162839ccb
Mushinako <8977737+Mushinako@users.noreply.github.com>
2020-10-19 16:48:56 -0700

    models

--- Projects/2 - Commerce/commerce/auctions/admin.py
+++ Projects/2 - Commerce/commerce/auctions/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Listing, Comment, Bid
+from .models import Listing, Comment, Bid, Category
 
 # Register your models here.
-for model in (Listing, Comment, Bid):
-    admin.site.register(model)
+admin.site.register(Listing)
+admin.site.register(Comment)
+admin.site.register(Bid)
+admin.site.register(Category)
--- Projects/2 - Commerce/commerce/auctions/models.py
+++ Projects/2 - Commerce/commerce/auctions/models.py
@@ -1,24 +1,50 @@
+from typing import Iterable, Optional
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Listing(models.Model):
+    active = models.BooleanField(default=True)
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=1024)
+    image_url = models.URLField(blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name="listings", blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+
 class User(AbstractUser):
+    watchlist = models.ManyToManyField(
+        Listing, related_name="watched_by", blank=True)
+
     def __str__(self) -> str:
         return f"{self.username}"
 
 
-class Listing(models.Model):
-    active = models.BooleanField()
+class Comment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments")
+    message = models.CharField(max_length=1024)
 
     def __str__(self) -> str:
-        return f"{self.id}: {self.active}"
+        return f"{self.user} commented {self.message}"
 
 
 class Bid(models.Model):
-    def __str__(self) -> str:
-        return f"{self.id}: {self}"
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(
+        Listing, on_delete=models.CASCADE, related_name="bids")
+    amount = models.PositiveIntegerField()
 
-
-class Comment(models.Model):
     def __str__(self) -> str:
-        return f"{self.id}: {self}"
+        return f"{self.user} bid {self.amount} on {self.listing}"
