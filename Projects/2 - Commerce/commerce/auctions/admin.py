46c27c92c125f0e7df7d16af09bb9b2dfa117fcd
Mushinako <8977737+Mushinako@users.noreply.github.com>
2020-10-20 19:21:04 -0700

    2 step 09

--- Projects/2 - Commerce/commerce/auctions/admin.py
+++ Projects/2 - Commerce/commerce/auctions/admin.py
@@ -7,28 +7,40 @@ from .models import Bid, Category, Comment, Listing, User, Watch
 
 class BidAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Bid._meta.fields]
+    list_editable = [field.name for field in Bid._meta.fields
+                     if field.name not in ("id", "creation_time")]
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Category._meta.fields]
+    list_editable = [field.name for field in Category._meta.fields
+                     if field.name not in ("id", "creation_time")]
 
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Comment._meta.fields]
+    list_editable = [field.name for field in Comment._meta.fields
+                     if field.name not in ("id", "creation_time")]
 
 
 class ListingAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Listing._meta.fields]
+    list_editable = [field.name for field in Listing._meta.fields
+                     if field.name not in ("id", "creation_time")]
     filter_horizontal = ("categories",)
 
 
 class UserAdmin(admin.ModelAdmin):
     list_display = [field.name for field in User._meta.fields
                     if field.name != "password"]
+    list_editable = [field.name for field in User._meta.fields
+                     if field.name not in ("id", "creation_time", "password")]
 
 
 class WatchAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Watch._meta.fields]
+    list_editable = [field.name for field in Watch._meta.fields
+                     if field.name not in ("id", "creation_time")]
 
 
 admin.site.register(Bid, BidAdmin)
--- Projects/2 - Commerce/commerce/auctions/views.py
+++ Projects/2 - Commerce/commerce/auctions/views.py
@@ -1,6 +1,9 @@
+from math import ceil
 from typing import Optional
+
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
+from django.db.models import Max
 from django.http import HttpResponseRedirect
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
@@ -12,7 +15,7 @@ from .models import Category, User, Listing, Watch, Bid
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    listings = Listing.objects.all()
+    listings = Listing.objects.filter(active=True)
     return render(request, "auctions/index.html", {
         "listings": listings,
     })
@@ -70,7 +73,7 @@ def register(request: HttpRequest) -> HttpResponse:
         return render(request, "auctions/register.html")
 
 
-def listing(request: HttpRequest, id_: str) -> HttpResponse:
+def listing(request: HttpRequest, id_: str, bid_err: bool = False) -> HttpResponse:
     """
     Listing details page
     """
@@ -87,15 +90,15 @@ def listing(request: HttpRequest, id_: str) -> HttpResponse:
     num_bids = all_bids.count()
     no_bids = num_bids == 0
     num_bids_str = f"{num_bids} bid{'' if num_bids == 1 else 's'}"
-    if all_bids:
-        max_bid = max(all_bids, key=lambda b: b.amount)
-        is_max_bid = max_bid.user == request.user
-        max_bid_amount = max_bid.amount
-    else:
+    max_bid = all_bids.order_by("-amount").first()
+    if max_bid is None:
         is_max_bid = False
         max_bid_amount = lt.starting_bid
+    else:
+        is_max_bid = max_bid.user == request.user
+        max_bid_amount = max_bid.amount
     max_bid_amount_str = f"{max_bid_amount/100:,.2f}"
-    min_starting_bid_str = f"{(max_bid_amount+1)/100:,.2f}"
+    min_starting_bid_str = f"{(max_bid_amount*1.1)/100:,.2f}"
     return render(request, "auctions/listing.html", {
         "lt": lt,
         "watching": watching,
@@ -106,6 +109,7 @@ def listing(request: HttpRequest, id_: str) -> HttpResponse:
         "max_bid": max_bid_amount_str,
         "min_starting_bid": min_starting_bid_str,
         "currency": "$",
+        "bid_err": bid_err,
     })
 
 
@@ -116,31 +120,31 @@ def watch(request: HttpRequest) -> HttpResponse:
     # Enforce "POST"
     if request.method != "POST":
         return _400(request)
-    action = request.POST.get("action", None)
-    id_ = request.POST.get("id_", None)
-    # Check parameter existence
-    if None in (action, id_):
-        return _400(request)
-    try:
-        id_ = int(id_)
-        lt = Listing.objects.get(id=id_)
-    except (ValueError, Listing.DoesNotExist) as err:
-        return _404(request, err)
     user = request.user
     # Check user is logged in
     if not user.is_authenticated:
         return _403(request)
+    id_str = request.POST.get("id_", None)
+    action = request.POST.get("action", None)
+    # Check parameter existence
+    if None in (id_str, action):
+        return _400(request)
+    try:
+        id_ = int(id_str)
+        lt = Listing.objects.get(id=id_)
+    except (ValueError, Listing.DoesNotExist) as err:
+        return _400(request, err)
     # Add to watchlist
     if action == "add":
         # Do not duplicate
         if not Watch.objects.filter(user=user, listing=lt).exists():
             watch = Watch(user=request.user, listing=lt)
             watch.save()
-        return HttpResponseRedirect(reverse("listing", kwargs={"id_": id_}))
+        return HttpResponseRedirect(reverse("listing", kwargs={"id_": id_str}))
     # Remove from watchlist
     elif action == "remove":
         Watch.objects.filter(user=user, listing=lt).delete()
-        return HttpResponseRedirect(reverse("listing", kwargs={"id_": id_}))
+        return HttpResponseRedirect(reverse("listing", kwargs={"id_": id_str}))
     else:
         return _400(request)
 
@@ -157,7 +161,8 @@ def category(request: HttpRequest, category: Optional[str] = None) -> HttpRespon
         })
     # Specific category
     else:
-        category_obj = Category.objects.filter(name=category).first()
+        category_obj = Category.objects.filter(
+            name=category, active=True).first()
         if category_obj is None:
             return _404(request)
         listings: Listing.objects = category_obj.listings
@@ -172,12 +177,65 @@ def bid(request: HttpRequest) -> HttpResponse:
     """
     Bid a listing
     """
+    # Enforce "POST"
+    if request.method != "POST":
+        return _400(request)
+    user = request.user
+    # Check user is logged in
+    if not user.is_authenticated:
+        return _403(request)
+    id_str = request.POST.get("id_", None)
+    bid_str = request.POST.get("bid", None)
+    # Check parameter existence
+    if None in (id_str, bid_str):
+        return _400(request)
+    try:
+        bid = ceil(float(bid_str)*100)
+        id_ = int(id_str)
+        lt = Listing.objects.get(id=id_)
+    except (ValueError, Listing.DoesNotExist) as err:
+        return _400(request, err)
+    # Check user is not the creator
+    if user == lt.created_by:
+        return _403(request)
+    bids: Bid.objects = lt.bids
+    max_bid_amount: Optional[int] = bids.aggregate(
+        Max("amount"))["amount__max"]
+    if max_bid_amount is None:
+        max_bid_amount: int = lt.starting_bid
+    # Check if the bid is larger than current
+    if bid <= max_bid_amount:
+        return HttpResponseRedirect(reverse("listing",
+                                            kwargs={"id_": id_str, "bid_err": True}))
+    # Add new bid
+    new_bid = Bid(user=user, listing=lt, amount=bid)
+    new_bid.save()
+    return HttpResponseRedirect(reverse("listing", kwargs={"id_": id_str}))
 
 
 def close(request: HttpRequest) -> HttpResponse:
     """
     Close a listing
     """
+    # Enforce "POST"
+    if request.method != "POST":
+        return _400(request)
+    id_str = request.POST.get("id_", None)
+    # Check parameter existence
+    if id_str == None:
+        return _400(request)
+    try:
+        id_ = int(id_str)
+        lt = Listing.objects.get(id=id_)
+    except (ValueError, Listing.DoesNotExist) as err:
+        return _400(request, err)
+    # Check user is the creator
+    if request.user != lt.created_by:
+        return _403(request)
+    # Mark nonactive
+    lt.active = False
+    lt.save()
+    return HttpResponseRedirect(reverse("listing", kwargs={"id_": id_str}))
 
 
 def _400(request: HttpRequest, exception=None) -> HttpResponse:
@@ -193,3 +251,6 @@ def _403(request: HttpRequest, exception=None) -> HttpResponse:
 def _404(request: HttpRequest, exception=None) -> HttpResponse:
     return page_not_found(request, exception)
     # return HttpResponseRedirect(reverse("index"))
+
+
+# TODO: Create listing; Comment
