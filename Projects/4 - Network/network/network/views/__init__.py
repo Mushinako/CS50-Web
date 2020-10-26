09ffb2bf9b592881df7300fb8e9d426dfd5f22b7
Mushinako <8977737+Mushinako@users.noreply.github.com>
2020-10-26 11:57:24 -0700

    4.06 view "like_unlike" part 1

--- Projects/4 - Network/network/network/views/__init__.py
+++ Projects/4 - Network/network/network/views/__init__.py
@@ -1,2 +1,2 @@
 from .auth import register, login_view, logout_view
-from .posts import index, new_post
+from .posts import index, new_post, like_unlike
--- Projects/4 - Network/network/network/views/posts.py
+++ Projects/4 - Network/network/network/views/posts.py
@@ -1,3 +1,6 @@
+import json
+from typing import Dict
+
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 
@@ -8,7 +11,14 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "network/index.html")
 
 
-def new_post(request: HttpRequest) -> HttpResponse:
+def new_post(request: HttpRequest) -> JsonResponse:
+    """
+    New post reciever
+     - request method not POST: 400
+     - user not logged in     : 403
+     - empty message content  : 400
+     - success                : 200
+    """
     if request.method != "POST":
         return JsonResponse({
             "msg": "Invalid request method.",
@@ -27,3 +37,35 @@ def new_post(request: HttpRequest) -> HttpResponse:
     return JsonResponse({
         "msg": "Success.",
     }, status=200)
+
+
+def like_unlike(request: HttpRequest) -> JsonResponse:
+    """
+    Like/unlike toggle
+     - request method not PUT: 400
+     - user not logged in    : 403
+     - invalid parameter     : 400
+     - success               : 200
+    """
+    if request.method != "PUT":
+        return JsonResponse({
+            "msg": "Invalid request method.",
+        }, status=400)
+    if not request.user.is_authenticated:
+        return JsonResponse({
+            "msg": "You have to log in to like/unlike",
+        }, status=403)
+    data: Dict[str, int] = json.loads(request.body)
+    post_id = data.get("postId", None)
+    status = data.get("status", None)
+    checks = (post_id, status)
+    if None in checks or not all(isinstance(c, int) for c in checks):
+        return JsonResponse({
+            "msg": "Empty message content.",
+        }, status=400)
+    if status:
+        # Add new like
+        pass
+    else:
+        # Remove existing likes
+        pass
