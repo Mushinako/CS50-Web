58795172cad98b4ecb7efc4dfbf2f6bc9509e4b3
Mushinako <8977737+Mushinako@users.noreply.github.com>
2020-10-26 13:54:54 -0700

    4.09 view "profile"

--- Projects/4 - Network/network/network/views/users.py
+++ Projects/4 - Network/network/network/views/users.py
@@ -4,7 +4,27 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 
+from ..models import User
+
 
 @login_required
 def profile(request: HttpRequest, username: Optional[str] = None) -> HttpResponse:
+    is_self = username is None
+    if is_self:
+        user = request.user
+    else:
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return render(request, "network/profile.html", {
+                "err": f"No user with username {username} exists.",
+            })
+    return render(request, "network/profile.html", {
+        "user_": user,
+        "is_self": is_self,
+    })
+
+
+@login_required
+def following(request: HttpRequest) -> HttpResponse:
     pass
