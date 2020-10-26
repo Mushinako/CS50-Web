9ae4e62c16439f53ddbee4055d9a7b38e5aefdc5
Mushinako <8977737+Mushinako@users.noreply.github.com>
2020-10-26 16:38:53 -0700

    4.11 HTML "profile"

---
+++ Projects/4 - Network/network/network/templatetags/check_followed.py
@@ -0,0 +1,17 @@
+from django import template
+
+from ..models import Follow, User
+
+register = template.Library()
+
+VISIBLE_CLASS_NAME = "button-visible"
+HIDDEN_CLASS_NAME = "button-hidden"
+
+
+@register.simple_tag()
+def followed_class(follower: User, followee: User, reverse: bool) -> str:
+    try:
+        Follow.objects.get(follower=follower, followee=followee)
+    except Follow.DoesNotExist:
+        reverse = not reverse
+    return " " + (HIDDEN_CLASS_NAME if reverse else VISIBLE_CLASS_NAME)
